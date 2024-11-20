9336194bed413a8b0f8b4788d45055ae4302cb47
Paras Shaha <paras.shaha@haptiq.com>
2024-11-20 17:34:29 +0530

    JS codes

---
+++ Iterators.js
@@ -0,0 +1,20 @@
+//Iterables
+
+const string = "Paras";
+
+// for (let value of string) {
+//   console.log(value);
+// }
+
+for (let i in string) {
+  console.log(string[i]);
+}
+
+const itra = {
+  name: "Paras",
+  id: 101,
+};
+
+for (let value in itra) {
+  console.log(itra[value]);
+}
---
+++ TemplateLiterals.js
@@ -0,0 +1,23 @@
+// name = `Paras`;
+// const hello = `Hello, ${name}`;
+
+// console.log(hello);
+
+// let arr = [1, 2, 3, 4, 5];
+
+// for (let value of arr) {
+//   console.log(value);
+// }
+
+// for (let value in arr) {
+//   console.log(value);
+// }
+
+const Upper = function(text){
+    console.log(text.toUpperCase());
+}
+
+const printNumber = function(number){
+    console.log(number);
+}
+  
